81e9b105bdf3c4b77e63e7d46381600caa3211b3
Dmitri Glusco <dima2492@gmail.com>
2019-08-17 20:25:47 +0300

    Added optimistic value

--- bandit/bandit.py
+++ bandit/bandit.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 class Bandit:
-    def __init__(self):
-        self.mean = 0
+    def __init__(self, mean: float = 0):
+        self.mean = mean
         self.N = 0
 
     def update(self, x):
         self.N += 1
         self.mean = (1 - 1.0 / self.N) * self.mean + (1.0 / self.N) * x
 
-        
